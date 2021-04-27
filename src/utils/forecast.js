ca875a6b98d70efdbb7950d2ae3f7b16314d189d
Parth Rane <parth.rane@ubisoft.com>
2021-04-27 22:33:48 +0530

    changed API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/69c681f946c21f3506ab349e4ecf8173/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
